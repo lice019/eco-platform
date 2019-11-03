56d69e4bf1f3ae78e34aa506eb664470f3dc63c5
lice <11606537852@qq.com>
2019-11-03 15:30:39 +0800

    eco-portal易购网门户网站架构搭建

---
+++ eco-portal/dist/js/index.js
@@ -0,0 +1,27 @@
+webpackJsonp([1],[
+/* 0 */
+/***/ (function(module, exports, __webpack_require__) {
+
+	module.exports = __webpack_require__(2);
+
+
+/***/ }),
+/* 1 */,
+/* 2 */
+/***/ (function(module, exports, __webpack_require__) {
+
+	
+	'use strict';
+	__webpack_require__(3);
+	__webpack_require__(4);
+	console.log("index");
+
+/***/ }),
+/* 3 */,
+/* 4 */
+/***/ (function(module, exports) {
+
+	// removed by extract-text-webpack-plugin
+
+/***/ })
+]);
---
+++ eco-portal/dist/js/login.js
@@ -0,0 +1,20 @@
+webpackJsonp([2],{
+
+/***/ 0:
+/***/ (function(module, exports, __webpack_require__) {
+
+	module.exports = __webpack_require__(8);
+
+
+/***/ }),
+
+/***/ 8:
+/***/ (function(module, exports, __webpack_require__) {
+
+	'use strict';
+	__webpack_require__(3);
+	console.log('hello login');
+
+/***/ })
+
+});
